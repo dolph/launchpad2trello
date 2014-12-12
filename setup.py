3afd6bea84e43b82644999601185c22b8d325860
Dolph Mathews <dolph.mathews@gmail.com>
2014-12-12 13:35:42 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+setuptools.setup(
+    name='launchpad2trello',
+    version='1.0.0',
+    description='Sync Launchpad activity to Trello',
+    author='Dolph Mathews',
+    author_email='dolph.mathews@gmail.com',
+    url='http://github.com/dolph/launchpad2trello',
+    packages=['launchpad2trello'],
+    entry_points={
+        'console_scripts': ['launchpad2trello = launchpad2trello:cli']},
+    install_requires=[
+        'argparse',
+        'requests',
+        'launchpadlib'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+    ],
+)
